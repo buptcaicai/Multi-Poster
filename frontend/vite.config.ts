615b06296782cab1dec789464bfeb4115e49ceeb
weiminli <lwm_bupt@163.com>
2025-05-05 23:15:19 +0930

    vite config to block chrome devtool request

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,22 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const chromeDevPlugin = () => ({
+  name: 'chromeDevPlugin',
+  configureServer(server: any) {
+    server.middlewares.use((req:any, res:any, next:any) => {
+      if (req.url === '/.well-known/appspecific/com.chrome.devtools.json') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+          devtools_page: 'http://localhost:9222/devtools/inspector.html'
+        }));
+      } else {
+        next();
+      }
+    })
+  }
+})
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), chromeDevPlugin()]
 });
